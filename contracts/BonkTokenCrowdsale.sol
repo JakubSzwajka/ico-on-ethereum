eb8e2a9b36ff450339e24b0c38870780e27472aa
Jakub Szwajka <jakubszwajka@Jakubs-MBP.lan>
2022-04-09 20:46:34 +0200

    BonkTokenCrowdsale constructor setup with proper imports

--- contracts/BonkTokenCrowdsale.sol
+++ contracts/BonkTokenCrowdsale.sol
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.5.5;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
 
 contract BonkTokenCrowdsale is Crowdsale {
 
+    constructor(uint256 _rate, address payable _wallet, IERC20 _token)
+    Crowdsale(_rate, _wallet, _token)
+    public 
+    {
+        
+    }
 
 }
