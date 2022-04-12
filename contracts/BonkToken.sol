f25c39bda397082919c5ef89d845789600ee0845
Jakub Szwajka <jakubszwajka@Jakubs-MacBook-Pro.local>
2022-04-12 18:00:46 +0200

    BonkToken as Ownable

--- contracts/BonkToken.sol
+++ contracts/BonkToken.sol
@@ -4,8 +4,9 @@ pragma solidity 0.5.5;
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 
-contract BonkToken is ERC20Mintable, ERC20Pausable, ERC20Detailed{
+contract BonkToken is ERC20Mintable, ERC20Pausable, ERC20Detailed, Ownable{
 
     constructor(string memory _name, string memory _symbol, uint8 _decimals) 
         ERC20Detailed(_name, _symbol, _decimals)
