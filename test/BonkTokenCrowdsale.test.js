6347adda9f4718112d84a880d4328eba336962d3
Jakub Szwajka <jakubszwajka@Jakubs-MacBook-Pro.local>
2022-04-12 18:02:09 +0200

    tests for mintable bonk token crowdsale added

--- test/BonkTokenCrowdsale.test.js
+++ test/BonkTokenCrowdsale.test.js
@@ -1,12 +1,16 @@
 
 
-const { BN } = require('@openzeppelin/test-helpers')
-const { expect } = require('chai')
+const { BN, ether } = require('@openzeppelin/test-helpers')
+const { expect, assert } = require('chai');
 
 const BonkToken = artifacts.require("BonkToken")
 const BonkTokenCrowdsale = artifacts.require("BonkTokenCrowdsale")
 
-contract('BonkTokenCrowdsale', function ([_, wallet]) {
+require('chai')
+  .use(require('chai-as-promised'))
+  .should();
+
+contract('BonkTokenCrowdsale', function ([_, wallet, investor_1, investor_2]) {
     
     beforeEach(async function () {
 
@@ -29,6 +33,9 @@ contract('BonkTokenCrowdsale', function ([_, wallet]) {
             this.wallet,
             this.token.address
         );
+        
+        await this.token.addMinter(this.crowdsale.address);
+        await this.token.transferOwnership(this.crowdsale.address);
     });
 
     describe('crowdsale', function () {
@@ -46,6 +53,23 @@ contract('BonkTokenCrowdsale', function ([_, wallet]) {
         it('tracks the token', async function () {
             expect(await this.crowdsale.token()).
                 to.equal(this.token.address);
+        });
+    });
+
+    describe('minted crowdsale', function () {
+        it('mints token after purchase', async function () {
+            const originalTotalSupply = await this.token.totalSupply();
+            await this.crowdsale.sendTransaction({ value: ether("1"), from: investor_1 });
+            const newTotalSupply = await this.token.totalSupply();
+
+            expect(newTotalSupply > originalTotalSupply).to.be.true;
+        });
+    });
+
+    describe('accepting payments', function () {
+        it('should accept payments', async function () {
+            await this.crowdsale.sendTransaction({ value: ether("1"), from: investor_1 }).should.be.fulfilled; 
+            await this.crowdsale.buyTokens(investor_1, { value: ether("1"), from: investor_2 }).should.be.fulfilled;
         });
     });
 });
