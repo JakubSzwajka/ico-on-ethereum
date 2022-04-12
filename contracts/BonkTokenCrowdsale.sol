7d1c699ed4c05bff3fd57d6df3eb8cec16ec763e
Jakub Szwajka <jakubszwajka@Jakubs-MacBook-Pro.local>
2022-04-12 18:01:49 +0200

    BonkTokenCrowdsale as mintable

--- contracts/BonkTokenCrowdsale.sol
+++ contracts/BonkTokenCrowdsale.sol
@@ -3,8 +3,9 @@ pragma solidity 0.5.5;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
+import "@openzeppelin/contracts/crowdsale/emission/MintedCrowdsale.sol"; 
 
-contract BonkTokenCrowdsale is Crowdsale {
+contract BonkTokenCrowdsale is Crowdsale, MintedCrowdsale {
 
     constructor(uint256 _rate, address payable _wallet, IERC20 _token)
     Crowdsale(_rate, _wallet, _token)
